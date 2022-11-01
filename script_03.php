a89027b7f9ae0969226fcad9c8a68086e2201157
Tanmay Vaij <tanmayvaij22@gmail.com>
2022-11-01 12:03:41 +0530

    added data types

--- script_03.php
+++ script_03.php
@@ -1,4 +1,4 @@
 <?php
     $var1 = "Simple String";
-    echo($var1);
+    echo $var1;
 ?>
--- script_04.php
+++ script_04.php
@@ -1,3 +1,26 @@
-<?
-        
+<?php
+
+    // String
+    $mystr = "Hello World";
+    echo var_dump($mystr);
+
+    // Integer
+    $myint = 255; 
+    echo var_dump($myint);
+
+    // Float
+    $myfloat = 45.55;
+    echo var_dump($myfloat);
+
+    // Boolean
+    $mybool = true;
+    echo var_dump($mybool);
+
+    // Array
+    $myarr = array(1,2,3);
+    echo var_dump($myarr);
+
+    $mynull = NULL;
+    echo var_dump($mynull);
+
 ?>
